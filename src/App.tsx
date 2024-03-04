aa01a573f051b576faf77515a730c2b75668fb15
Martin Macharia <mosmartin5@gmail.com>
2024-03-04 22:04:27 +0100

    Add components to App, Container, Header, and Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import Background from "./components/Background";
+import Container from "./components/Container";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 function App() {
-  return <>Hello!</>;
+  return (
+    <>
+      <Background />
+
+      <Header />
+
+      <Container />
+
+      <Footer />
+    </>
+  );
 }
 
 export default App;
--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,3 +1,12 @@
+import JobItemContent from "./JobItemContent";
+import Sidebar from "./Sidebar";
+
 export default function Container() {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <Sidebar />
+
+      <JobItemContent />
+    </div>
+  );
 }
